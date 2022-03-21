b7a4b1e6b4eae35c11aa9586b63fe09d73c7934a
Blake Oliveira <blake@blakelee.net>
2022-03-20 22:11:32 -0700

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,68 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.jetbrains.compose)
+    alias(libs.plugins.kobweb.application)
+    alias(libs.plugins.kobwebx.markdown)
+}
+
+repositories {
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    google()
+    maven("https://us-central1-maven.pkg.dev/varabyte-repos/public")
+}
+
+group = "net.blakelee.site"
+version = "1.0-SNAPSHOT"
+
+kobweb {
+    index {
+        description.set("Powered by Kobweb")
+    }
+}
+
+kotlin {
+    jvm {
+        tasks.withType<KotlinCompile> {
+            kotlinOptions.jvmTarget = "11"
+        }
+
+        tasks.named("jvmJar", Jar::class.java).configure {
+            archiveFileName.set("site.jar")
+        }
+    }
+    js(IR) {
+        moduleName = "site"
+        browser {
+            commonWebpackConfig {
+                outputFileName = "site.js"
+            }
+        }
+        binaries.executable()
+    }
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(compose.runtime)
+            }
+        }
+
+        val jsMain by getting {
+            dependencies {
+                implementation(compose.web.core)
+                implementation(libs.kobweb.core)
+                implementation(libs.kobweb.silk.core)
+                implementation(libs.kobweb.silk.icons.fa)
+                implementation(libs.kobwebx.markdown)
+             }
+        }
+
+        val jvmMain by getting {
+            dependencies {
+                implementation(libs.kobweb.api)
+             }
+        }
+    }
+}
---
+++ src/jsMain/kotlin/net/blakelee/site/pages/About.kt
@@ -0,0 +1,18 @@
+package net.blakelee.site.pages
+
+import androidx.compose.runtime.Composable
+import com.varabyte.kobweb.core.Page
+import com.varabyte.kobweb.silk.components.navigation.Link
+import com.varabyte.kobweb.silk.components.text.Text
+import net.blakelee.site.components.layouts.PageLayout
+import org.jetbrains.compose.web.dom.P
+
+@Page
+@Composable
+fun AboutPage() {
+    PageLayout("ABOUT") {
+        Text("This is a skeleton app used to showcase a basic site made using Kobweb")
+        P()
+        Link("/", "Go Home")
+    }
+}
